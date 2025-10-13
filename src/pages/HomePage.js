729f3e4623840e3c2039426c0aae65b7d6c73dbc
Isabelle <ilstone17@gmail.com>
2025-10-13 15:42:30 -0400

    adding fonts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,17 +2,24 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div className='min-h-screen bg-gradient-to-br from-blue-400 to-purple-500'>
+        <div className='min-h-screen bg-gradient-to-br from-green-400 to-emerald-600'>
             {/* NAVIGATION BAR */}
-            <nav className='bg-white shadow-md p-4'>
+            <nav className='bg-stone-100 shadow-md p-4'>
                 <div className='flex justify-between items-center px-8'>
-                    <h2 className='text-2xl font-bold text-gray-800'>SerendiQuest</h2>
+                    {/* Left Links: */}
                     <div className='flex gap-6'>
-                        <a href ='/' className='text-gray-700 hover:text-blue-500'>Home</a>
-                        <a href='/questgenerator' className='text-gray-700 hover:text-blue-500'>Quest Generator</a>
-                        <a href='/profile' className='text-gray-700 hover:text-blue-500'>Profile</a>
-                        <a href='/login' className='text-gray-700 hover:text-blue-500'>Login</a>
-                        <a href='/signup' className='text-gray-700 hover:text-blue-500'>Sign Up</a>
+                        <a href='/about' className='text-stone-700 hover:text-green-600'>About</a>
+                        <a href='/login' className='text-stone-700 hover:text-green-600'>Login/Sign Up</a>
+                    </div>
+
+                    {/* Center Link: */}
+                    <a href='/' className='text-2xl font-bold text-green-800hover:text-green-600'>SerendiQuest</a>
+                    
+                    {/* Right Links: */}
+                    <div className='flex gap-6'>
+                        <a href='/questgenerator' className='text-stone-700 hover:text-green-600'>Quest Generator</a>
+                        <a href='/profile' className='text-stone-700 hover:text-green-600'>Profile</a>
+            
                     </div>
                 </div>
             </nav>
