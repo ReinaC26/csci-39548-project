eba46903ff0c2dd813536fd782cacc33c8a6d1e3
Isabelle <ilstone17@gmail.com>
2025-10-13 15:44:30 -0400

    adding fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}"
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'game': ['"Press Start 2P"', 'cursive'], 
+                'mono': ['"Kode Mono"', 'monospace'],
+            }
+        },
     },
     plugins: [],
 }
