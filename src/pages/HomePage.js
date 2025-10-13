5df041171ecf3af921be090e93fc1fdf69c63a39
Isabelle <ilstone17@gmail.com>
2025-10-13 16:13:30 -0400

    style changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div className='min-h-screen bg-gradient-to-br from-green-400 to-emerald-600'>
+        <div className='min-h-screen bg-black'>
             {/* NAVIGATION BAR */}
-            <nav className='bg-stone-100 shadow-md p-4'>
-                <div className='flex justify-between items-center px-8'>
+            <nav className='bg-black shadow-md p-4'>
+                <div className='flex justify-between items-center px-10'>
                     {/* Left Links: */}
-                    <div className='flex gap-6'>
-                        <a href='/about' className='text-stone-700 hover:text-green-600'>About</a>
-                        <a href='/login' className='text-stone-700 hover:text-green-600'>Login/Sign Up</a>
+                    <div className='flex gap-6 font-game'>
+                        <a href='/about' className='text-green-500 hover:text-green-400'>About</a>
+                        <a href='/login' className='text-green-500 hover:text-green-400'>Login/Sign Up</a>
                     </div>
 
                     {/* Center Link: */}
-                    <a href='/' className='text-2xl font-bold text-green-800hover:text-green-600'>SerendiQuest</a>
+                    <a href='/' className='text-2xl font-bold text-green-500 hover:text-green-400 font-game'>SerendiQuest</a>
                     
                     {/* Right Links: */}
-                    <div className='flex gap-6'>
-                        <a href='/questgenerator' className='text-stone-700 hover:text-green-600'>Quest Generator</a>
-                        <a href='/profile' className='text-stone-700 hover:text-green-600'>Profile</a>
+                    <div className='flex gap-6 font-game'>
+                        <a href='/questgenerator' className='text-green-500 hover:text-green-400'>Quest Generator</a>
+                        <a href='/profile' className='text-green-500 hover:text-green-400'>Profile</a>
             
                     </div>
                 </div>
