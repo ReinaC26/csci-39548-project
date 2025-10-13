7093b1034841db82f8432b4817d348541e3f5ccd
Isabelle <ilstone17@gmail.com>
2025-10-13 13:40:27 -0400

    Simple HomePage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div style={{ textAlign: 'center', padding: '50px' }}>
+            <h1>SerindiQuest!!!!!!</h1>
+            <button style={{
+                padding: '10px 20px',
+                fontSize: '16px',
+                cursor: 'pointer',
+                marginTop: '20px'
+            }}>
+                Adventure WOOOOO
+            </button>
+        </div>
+    );
+}
+
+export default HomePage;
