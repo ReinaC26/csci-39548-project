658f03e0d35d2f62f2bb69c557e6d778f2eaa6b3
youth5427 <SUHYEON.YOO33@myhunter.cuny.edu>
2025-12-19 17:19:54 -0500

    connect sign up with bacend

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
+const API_BASE_URL = "http://localhost:5002";
+
 function SignupForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirm, setConfirm] = useState("");
+
   const [isMatch, setIsMatch] = useState(null);
+
+  const navigate = useNavigate();
 
   // check between password and confirm password
   useEffect(() => {
@@ -15,16 +23,67 @@ function SignupForm() {
     setIsMatch(password === confirm);
   }, [password, confirm]);
 
+  // Sin-up form Submit
+  const handleSubmit = async (e) => {
+    // prevent collison with previous log
+    e.preventDefault();
+
+    // check filled out
+    if (!username || !email || !password) {
+      alert("Please fill out all fields.");
+
+      return;
+    }
+    if (password !== confirm) {
+      return;
+    }
+
+    try {
+      const res = await fetch(`${API_BASE_URL}/api/auth/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, email, password }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data?.message);
+      }
+
+      // if success, save token
+      if (data?.token) {
+        localStorage.setItem("token", data.token);
+      }
+      alert("Registered successfully!");
+      navigate("/login");
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={handleSubmit}>
       <h2 className="login-subtitle">Sign-up</h2>
       <hr className="divider" />
 
       <label>Name</label>
-      <input type="text" placeholder="Username" required />
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
 
       <label>Email</label>
-      <input type="email" placeholder="address@example.com" required />
+      <input
+        type="email"
+        placeholder="address@example.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
 
       <label>Password</label>
       <input
