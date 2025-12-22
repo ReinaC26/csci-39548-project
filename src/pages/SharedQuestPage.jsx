da6c011505705e48a3b31ad41218e4a19b51c22f
bethanykeith <69405430+bethanykeith@users.noreply.github.com>
2025-12-22 09:05:35 -0500

    quest encoding edit

--- src/pages/SharedQuestPage.jsx
+++ src/pages/SharedQuestPage.jsx
@@ -1,5 +1,13 @@
 import QuestGeneratorPage from "./QuestGeneratorPage";
 
+function decodeQuest(str) {
+  const base64 = str
+    .replace(/-/g, "+")
+    .replace(/_/g, "/");
+
+  return JSON.parse(atob(base64));
+}
+
 function SharedQuestPage() {
   const params = new URLSearchParams(window.location.search);
   const data = params.get("data");
@@ -8,18 +16,25 @@ function SharedQuestPage() {
 
   let quest;
   try {
-    quest = JSON.parse(atob(decodeURIComponent(data)));
+    quest = decodeQuest(data);
     console.log("Decoded quest:", quest);
-
-  } catch {
+  } catch (err) {
+    console.error("Quest decode failed", err);
     return <div>Invalid quest data.</div>;
   }
 
   return (
     <QuestGeneratorPage
-      initialQuest={quest}
-      readOnly={true}
-      sender = {quest.sender} //references sender of shared quest
+      initialQuest={{    
+        startLocation: quest.startLocation,
+        endLocation: quest.endLocation,
+        distance: quest.distance,
+        duration: quest.duration,
+        description: quest.description,
+        questGoal: quest.questGoal,
+        completed: true,}}
+      readOnly
+      sender={quest.sender}
     />
   );
 }
